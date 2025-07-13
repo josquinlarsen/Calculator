17a29002d35d2bb56aced8beab2bca40d5a839c8
Josquin Larsen <jsqnlrsn@gmail.com>
2025-07-13 10:05:13 -0400

    update result layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,24 @@ function App() {
 
   const [result, setResult] = useState([]);
 
+  const displayResult = (
+    <>
+    <div className='resultsDisplay'>
+      {result.map((item, idx)=> (
+        <p key={idx}>{(idx < 3) ? item + '.' : item}</p>
+      ))}
+    </div>
+    </>
+  )
+
   return (
     <>
       <h1>Subnet IPv4 Calculator</h1>
       <CalculatorForm
         setResult={setResult} />
       <div>
-        {(result.length > 0) && (
-          result.map((item) => (
-            <p key={Math.random()}>{item}</p>
-          )))}
+        {(result.length > 0) && 
+        (displayResult)}
       </div>
 
     </>
